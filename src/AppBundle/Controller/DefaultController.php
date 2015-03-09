6f218d6ee0f66d73a8726733ec84fb2d6d11425e
dm84 <dmitriy.o84@ya.ru>
2015-03-09 21:21:46 +0600

    +страница авторизации

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -14,4 +14,23 @@ class DefaultController extends Controller
     {
         return $this->render('default/index.html.twig');
     }
+    
+    /**
+     * @Route("/login", name="login")
+     */
+    public function loginAction()
+    {
+    	return $this->render('default/login.html.twig', [
+    			"signin_url" => "",
+    			"facebook_signin_url" => $this->generateUrl('hwi_oauth_service_redirect', ["service" => "facebook"]),
+    			"login" => "",
+    			"login_placeholder" => "Ваш логин",
+    			"pwd_placeholder" => "Ваш пароль",
+    			"pwd_confirm_placeholder" => "Ваш пароль еще раз",
+    			"signup_header" => "Регистрация",    			
+    			"signup_url" => "",    			
+    			"signup_caption" => "Зарегистрироваться",    			
+    			"js_path" => "/bundles/app/js"
+    	]);
+    }
 }
